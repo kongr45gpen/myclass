2b6e2720615bcf408b5ddcf2926caa2b69a1ec0d
kongr45gpen <electrovesta@gmail.com>
2014-08-13 15:28:12 +0300

    Rename DefaultController to LandingController

--- src/Helit/Bundle/MyClassBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Helit/Bundle/MyClassBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,14 +4,14 @@ namespace Helit\Bundle\MyClassBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class LandingControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Welcome")')->count() > 0);
     }
 }
