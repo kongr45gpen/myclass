50bb1fbdef7eafbb7c541f23f2bb406d6fbfdb9a
kongr45gpen <kongr45gpen@helit.org>
2014-09-11 15:07:25 +0300

    Add basic entities for teachers, classes and schedule

---
+++ src/Helit/Bundle/MyClassBundle/Entity/Orientation.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Helit\Bundle\MyClassBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Orientation
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Orientation
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="full_name", type="string", length=255)
+     */
+    private $fullName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Orientation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set fullName
+     *
+     * @param string $fullName
+     * @return Orientation
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * Get fullName
+     *
+     * @return string 
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+}
---
+++ src/Helit/Bundle/MyClassBundle/Entity/SchoolClass.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Helit\Bundle\MyClassBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SchoolClass
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class SchoolClass
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=10)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return SchoolClass
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
