ef1c3169777fcfdd5df0d8f5bd0c6fb7b272cb9e
kongr45gpen <electrovesta@gmail.com>
2014-09-15 15:46:13 +0300

    Show list of classes on the landing page

--- src/Helit/Bundle/MyClassBundle/Controller/LandingController.php
+++ src/Helit/Bundle/MyClassBundle/Controller/LandingController.php
@@ -14,6 +14,24 @@ class LandingController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        // $repository = $this->getDoctrine()
+            // ->getRepository('HelitMyClassBundle:Separation');
+
+        // $result = $repository->createQueryBuilder('s')
+            // ->where();
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT c FROM HelitMyClassBundle:SchoolClass c
+            JOIN c.orientation or
+            JOIN or.separation s
+            JOIN s.orientations o
+            GROUP BY c.id
+            HAVING count(o) = 1'
+        )->getResult();
+
+        $separations = $em->getRepository('HelitMyClassBundle:Separation')->findAll();
+
+        return array('classes' => $query, 'separations' => $separations);
     }
 }
