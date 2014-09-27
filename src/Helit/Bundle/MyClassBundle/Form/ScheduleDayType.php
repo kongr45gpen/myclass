42eb7a54b4f238c9e7c9b3ac6ddf24953fec663a
kongr45gpen <electrovesta@gmail.com>
2014-09-27 15:15:40 +0300

    Add buttons and visual cues for active schedule items on the edit form

--- src/Helit/Bundle/MyClassBundle/Form/ScheduleDayType.php
+++ src/Helit/Bundle/MyClassBundle/Form/ScheduleDayType.php
@@ -2,6 +2,7 @@
 
 namespace Helit\Bundle\MyClassBundle\Form;
 
+use Helit\Bundle\MyClassBundle\Entity\ScheduleItem;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -19,10 +20,20 @@ class ScheduleDayType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         foreach ($this->schedule as $hour => $item) {
+            if ($item->getStatus() == ScheduleItem::STATUS_UNKNOWN) {
+                $class = 'negative';
+            } elseif ($item->getClass() !== null) {
+                $class = 'active';
+            } else {
+                $class = '';
+            }
+
             $builder->add(
                 $builder->create(
                     'hour_'.$hour,
-                    new ScheduleItemType())->setData($item)
+                    new ScheduleItemType(),
+                    ['attr' => ['class' => $class]]
+                )->setData($item)
             );
         }
     }
